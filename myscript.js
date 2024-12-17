4dd7ba26a72d0bd7a4518688b5ccc97f5e414726
Asunder <trashemail3301@gmail.com>
2024-12-17 10:06:03 -0800

    operator buttons and operate function
    added buttons for all numbers and operators. Aswell as create operate function that takes in two numbers and an operator and calls one of the math functions.

--- myscript.js
+++ myscript.js
@@ -1,31 +1,50 @@
-let firstNumber = 0;
-secondNumber = 0;
-operator = x;
+let firstNumber = '';
+secondNumber = '';
+mathSign = '';
 
 function add (a,b) {
     return a + b;
     firstNumber = a;
     secondNumber = b;
-    operator = '+'
+    mathSign = '-'
 }
 
 function subtract (a, b) {
     return a - b;
     firstNumber = a;
     secondNumber = b;
-    operator = '-'
+    mathSign = '-'
 }
 
 function multiply (a, b) {
     return a * b;
     firstNumber = a;
     secondNumber = b;
-    operator = 'x';
+    mathSign = 'x';
 }
 
 function divide (a, b) {
-    return a / b;
+    if (a === 0) {
+        alert(`CAN'T DIVIDE BY ZERO`);
+    } else {
+        return a / b;
+    }
     firstNumber = a;
     secondNumber = b
-    operator = '/'
+    mathSign = '/'
+}
+
+function operate (operator, num1, num2) {
+    if (operator === '+') {
+        return add(num1, num2);
+    } else if (operator === '-') {
+        return subtract(num1, num1);
+    } else if (operator === '*') {
+        return multiply(num1, num2);
+    } else if (operator === '/') {
+        return divide(num1, num2);
+    } else {
+        return alert('INVALID OPERATOR');
+    }
+
 }
